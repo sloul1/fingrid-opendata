78e2aa9c2ab549633ee24f0bdaadf5681a551855
Petri Heikkinen <p.j.hektinen@gmail.com>
2024-06-20 01:21:28 +0300

    added API active check and checked that error message and program exiting works by blocking DNS, some edits in parameter inputs

--- app.py
+++ app.py
@@ -5,22 +5,43 @@ import urllib.request, json, os
 from dotenv import load_dotenv
 load_dotenv()
 
+
 # Read API key from .env file
 api_key = os.getenv('API_KEY')
 if api_key is None:
     raise Exception("Failed to load API key from .env file")
 
-# ToDo: Determine if api is active at  
-# url = "https://data.fingrid.fi/api/notifications/active"
-# before making queries!!
+# Test if Fingrid API is active before making queries.
+try:
+    url = "https://data.fingrid.fi/api/notifications/active"
 
-# Get user input for each parameter
+    hdr ={
+    # Request headers
+    'Cache-Control': 'no-cache',
+    'x-api-key': api_key,
+    }
+
+    req = urllib.request.Request(url, headers=hdr)
+
+    req.get_method = lambda: 'GET'
+    response = urllib.request.urlopen(req)
+    if response.getcode() == 200:
+        print("Fingrid API is active.")
+    else:
+        raise Exception(f"Unexpected status code: {response.getcode()}")
+except Exception as e:
+    print(f"Error when checking API availability: {e}")
+    exit()
+# Exit if the API is not available
+
+# Get user input for parameters
+print("Available dataset IDs are listed in https://data.fingrid.fi/en/datasets")
 dataset_id = input("Enter dataset ID: ")
 start_time = input("Enter start time (format: YYYY-MM-DDTHH:MM:SSZ): ")
 end_time = input("Enter end time (format: YYYY-MM-DDTHH:MM:SSZ): ")
 format_ = input("Enter desired output format (json, csv or xml): ")
 one_row_per_time_period = input("Do you want one row per time period? (yes/no): ")
-page_size = int(input("Enter page size: "))
+page_size = int(input("How many results per page?: "))
 locale = input("Enter locale (en/fi): ")
 sort_by = input("Enter sort by (startTime/endtime): ")
 sort_order = input("Enter sort order (asc/desc): ")
